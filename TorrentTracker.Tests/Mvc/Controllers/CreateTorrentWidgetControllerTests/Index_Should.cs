f1e3482f2e880b99049bf79e8d5741a28c45a3a1
annix8 <mario.ivailov@abv.bg>
2019-04-04 23:04:31 +0300

    added unit tests for controller

---
+++ TorrentTracker.Tests/Mvc/Controllers/CreateTorrentWidgetControllerTests/Index_Should.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SitefinityWebApp.Mvc.Controllers;
+using SitefinityWebApp.Mvc.Models;
+using SitefinityWebApp.TorrentTrackerServices.Contracts;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TorrentTracker.Tests.Mvc.Controllers.CreateTorrentWidgetControllerTests
+{
+    [TestClass]
+    public class Index_Should
+    {
+        private Mock<ITorrentService> _torrentServiceStub;
+        private Mock<ITaxonomyService> _taxonomyServiceStub;
+        private Mock<IModelStateValidatorService> _modelStateValidatorServiceStub;
+        private Mock<HttpPostedFileBase> _userImageDataStub;
+        private Mock<HttpPostedFileBase> _userTorrentDataStub;
+        private CreateTorrentWidgetController _controller;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _torrentServiceStub = new Mock<ITorrentService>();
+            _taxonomyServiceStub = new Mock<ITaxonomyService>();
+            _modelStateValidatorServiceStub = new Mock<IModelStateValidatorService>();
+            _userImageDataStub = new Mock<HttpPostedFileBase>();
+            _userTorrentDataStub = new Mock<HttpPostedFileBase>();
+            _controller = new CreateTorrentWidgetController(_torrentServiceStub.Object,
+                _taxonomyServiceStub.Object,
+                _modelStateValidatorServiceStub.Object);
+        }
+
+        [TestMethod]
+        public void AddSuccessMessageToTempData_When_ModelStateIsValid()
+        {
+            _modelStateValidatorServiceStub.Setup(x => x.Validate(It.IsAny<ModelStateDictionary>(), It.IsAny<CreateTorrentWidgetModel>()))
+                .Returns(true);
+
+            var torrentWidgetModel = CreateTorrentWidgetModel();
+
+            _controller.Index(torrentWidgetModel);
+
+            Assert.IsTrue(_controller.TempData.ContainsKey(SitefinityWebApp.Constants.SuccessMessageKey));
+        }
+
+        [TestMethod]
+        public void AddErrorMessageToTempData_When_ModelStateIsValidAndTorrentModelIsNull()
+        {
+            _modelStateValidatorServiceStub.Setup(x => x.Validate(It.IsAny<ModelStateDictionary>(), It.IsAny<CreateTorrentWidgetModel>()))
+                .Returns(true);
+
+            CreateTorrentWidgetModel torrentWidgetModel = null;
+
+            _controller.Index(torrentWidgetModel);
+
+            Assert.IsTrue(_controller.TempData.ContainsKey(SitefinityWebApp.Constants.ErrorMessageKey));
+        }
+
+        private CreateTorrentWidgetModel CreateTorrentWidgetModel()
+        {
+            return new CreateTorrentWidgetModel()
+            {
+                Title = string.Empty,
+                AdditionalInfo = string.Empty,
+                Description = string.Empty,
+                Genres = new List<string>(),
+                UserImageData = _userImageDataStub.Object,
+                UserTorrentData = _userTorrentDataStub.Object
+            };
+        }
+    }
+}
