751482e74ca844577befbc506eda403c89ac05c9
annix8 <mario.ivailov@abv.bg>
2019-04-04 22:44:54 +0300

    added unit tests to cover the file extension validations

---
+++ TorrentTracker.Tests/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace TorrentTracker.Tests
+{
+    public static class Constants
+    {
+        public const string JpegExtension = ".jpeg";
+        public const string TorrentExtension = ".torrent";
+        public const string InvalidExtension = ".txt";
+    }
+}
--- TorrentTracker.Tests/TorrentTrackerServices/ModelStateValidatorServiceTests/Validate_Should.cs
+++ TorrentTracker.Tests/TorrentTrackerServices/ModelStateValidatorServiceTests/Validate_Should.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SitefinityWebApp.Facades.Contracts;
+using SitefinityWebApp.Mvc.Models;
 using SitefinityWebApp.TorrentTrackerServices;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 
 namespace TorrentTracker.Tests.TorrentTrackerServices.ModelStateValidatorServiceTests
@@ -15,12 +12,16 @@ namespace TorrentTracker.Tests.TorrentTrackerServices.ModelStateValidatorService
     public class Validate_Should
     {
         private Mock<IPathFacade> _pathFacadeStub;
+        private Mock<HttpPostedFileBase> _userImageDataStub;
+        private Mock<HttpPostedFileBase> _userTorrentDataStub;
         private ModelStateValidatorService _modelStateValidatorService;
 
         [TestInitialize]
         public void Init()
         {
             _pathFacadeStub = new Mock<IPathFacade>();
+            _userImageDataStub = new Mock<HttpPostedFileBase>();
+            _userTorrentDataStub = new Mock<HttpPostedFileBase>();
             _modelStateValidatorService = new ModelStateValidatorService(_pathFacadeStub.Object);
         }
 
@@ -31,6 +32,60 @@ namespace TorrentTracker.Tests.TorrentTrackerServices.ModelStateValidatorService
             modelState.AddModelError(string.Empty, "dummy error");
 
             bool actual = _modelStateValidatorService.Validate(modelState, null);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ReturnTrue_When_ModelStateIsValidAndFilesAreValid()
+        {
+            var modelState = new ModelStateDictionary();
+            _userImageDataStub.Setup(x => x.FileName).Returns(Constants.JpegExtension);
+            _userTorrentDataStub.Setup(x => x.FileName).Returns(Constants.TorrentExtension);
+            var torrentWidgetModel = new CreateTorrentWidgetModel()
+            {
+                UserImageData = _userImageDataStub.Object,
+                UserTorrentData = _userTorrentDataStub.Object
+            };
+            _pathFacadeStub.SetupSequence(x => x.GetExtension(It.IsAny<string>()))
+                .Returns(torrentWidgetModel.UserImageData.FileName).Returns(torrentWidgetModel.UserTorrentData.FileName);
+
+            bool actual = _modelStateValidatorService.Validate(modelState, torrentWidgetModel);
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_When_ModelStateIsValidAndImageFileIsInvalid()
+        {
+            var modelState = new ModelStateDictionary();
+            _userImageDataStub.Setup(x => x.FileName).Returns(Constants.InvalidExtension);
+            _userTorrentDataStub.Setup(x => x.FileName).Returns(Constants.TorrentExtension);
+            var torrentWidgetModel = new CreateTorrentWidgetModel()
+            {
+                UserImageData = _userImageDataStub.Object,
+                UserTorrentData = _userTorrentDataStub.Object
+            };
+            _pathFacadeStub.SetupSequence(x => x.GetExtension(It.IsAny<string>()))
+                .Returns(torrentWidgetModel.UserImageData.FileName).Returns(torrentWidgetModel.UserTorrentData.FileName);
+
+            bool actual = _modelStateValidatorService.Validate(modelState, torrentWidgetModel);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_When_ModelStateIsValidAndTorrentFileIsInvalid()
+        {
+            var modelState = new ModelStateDictionary();
+            _userImageDataStub.Setup(x => x.FileName).Returns(Constants.JpegExtension);
+            _userTorrentDataStub.Setup(x => x.FileName).Returns(Constants.InvalidExtension);
+            var torrentWidgetModel = new CreateTorrentWidgetModel()
+            {
+                UserImageData = _userImageDataStub.Object,
+                UserTorrentData = _userTorrentDataStub.Object
+            };
+            _pathFacadeStub.SetupSequence(x => x.GetExtension(It.IsAny<string>()))
+                .Returns(torrentWidgetModel.UserImageData.FileName).Returns(torrentWidgetModel.UserTorrentData.FileName);
+
+            bool actual = _modelStateValidatorService.Validate(modelState, torrentWidgetModel);
             Assert.IsFalse(actual);
         }
     }
