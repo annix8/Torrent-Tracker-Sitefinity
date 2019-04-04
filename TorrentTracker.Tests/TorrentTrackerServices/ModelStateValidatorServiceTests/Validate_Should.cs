4f925eacd653847b6d79714a6204d7124408f08c
annix8 <mario.ivailov@abv.bg>
2019-04-04 22:30:44 +0300

    added unit test project

---
+++ TorrentTracker.Tests/TorrentTrackerServices/ModelStateValidatorServiceTests/Validate_Should.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SitefinityWebApp.Facades.Contracts;
+using SitefinityWebApp.TorrentTrackerServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace TorrentTracker.Tests.TorrentTrackerServices.ModelStateValidatorServiceTests
+{
+    [TestClass]
+    public class Validate_Should
+    {
+        private Mock<IPathFacade> _pathFacadeStub;
+        private ModelStateValidatorService _modelStateValidatorService;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _pathFacadeStub = new Mock<IPathFacade>();
+            _modelStateValidatorService = new ModelStateValidatorService(_pathFacadeStub.Object);
+        }
+
+        [TestMethod]
+        public void ReturnFalse_When_ModelStateIsInvalid()
+        {
+            var modelState = new ModelStateDictionary();
+            modelState.AddModelError(string.Empty, "dummy error");
+
+            bool actual = _modelStateValidatorService.Validate(modelState, null);
+            Assert.IsFalse(actual);
+        }
+    }
+}
